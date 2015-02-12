5c91f3fd081e6805955637b391c0a0429c45e0e0
bsx <bsx+ccc@0xcafec0.de>
2015-02-12 20:35:36 +0100

    fix(handlers): insert correct irq handlers

--- mvp/handlers.c
+++ mvp/handlers.c
@@ -42,24 +42,88 @@ void irq_undefined() {
   while(1);
 }
 
-void I2C_IRQHandler(void)       WEAK_ALIAS(irq_undefined)
-void TIMER16_0_IRQHandler(void) WEAK_ALIAS(irq_undefined)
-void TIMER16_1_IRQHandler(void) WEAK_ALIAS(irq_undefined)
-void TIMER32_0_IRQHandler(void) WEAK_ALIAS(irq_undefined)
-void TIMER32_1_IRQHandler(void) WEAK_ALIAS(irq_undefined)
-void SSP_IRQHandler(void)       WEAK_ALIAS(irq_undefined)
-void UART_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
-void USB_IRQHandler(void)       WEAK_ALIAS(irq_undefined)
-void USB_FIQHandler(void)       WEAK_ALIAS(irq_undefined)
+void WWDG_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void PVD_IRQHandler(void)       WEAK_ALIAS(irq_undefined)
+void TAMP_STAMP_IRQHandler(void)        WEAK_ALIAS(irq_undefined)
+void RTC_WKUP_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void FLASH_IRQHandler(void)     WEAK_ALIAS(irq_undefined)
+void RCC_IRQHandler(void)       WEAK_ALIAS(irq_undefined)
+void EXTI0_IRQHandler(void)     WEAK_ALIAS(irq_undefined)
+void EXTI1_IRQHandler(void)     WEAK_ALIAS(irq_undefined)
+void EXTI2_IRQHandler(void)     WEAK_ALIAS(irq_undefined)
+void EXTI3_IRQHandler(void)     WEAK_ALIAS(irq_undefined)
+void EXTI4_IRQHandler(void)     WEAK_ALIAS(irq_undefined)
+void DMA1_Stream0_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void DMA1_Stream1_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void DMA1_Stream2_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void DMA1_Stream3_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void DMA1_Stream4_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void DMA1_Stream5_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void DMA1_Stream6_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
 void ADC_IRQHandler(void)       WEAK_ALIAS(irq_undefined)
-void WDT_IRQHandler(void)       WEAK_ALIAS(irq_undefined)
-void BOD_IRQHandler(void)       WEAK_ALIAS(irq_undefined)
-void FMC_IRQHandler(void)       WEAK_ALIAS(irq_undefined)
-void PIOINT3_IRQHandler(void)   WEAK_ALIAS(irq_undefined)
-void PIOINT2_IRQHandler(void)   WEAK_ALIAS(irq_undefined)
-void PIOINT1_IRQHandler(void)   WEAK_ALIAS(irq_undefined)
-void PIOINT0_IRQHandler(void)   WEAK_ALIAS(irq_undefined)
-void WAKEUP_IRQHandler(void)    WEAK_ALIAS(irq_undefined)
+void CAN1_TX_IRQHandler(void)       WEAK_ALIAS(irq_undefined)
+void CAN1_RX0_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void CAN1_RX1_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void CAN1_SCE_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void EXTI9_5_IRQHandler(void)       WEAK_ALIAS(irq_undefined)
+void TIM1_BRK_TIM9_IRQHandler(void)     WEAK_ALIAS(irq_undefined)
+void TIM1_UP_TIM10_IRQHandler(void)     WEAK_ALIAS(irq_undefined)
+void TIM1_TRG_COM_TIM11_IRQHandler(void)        WEAK_ALIAS(irq_undefined)
+void TIM1_CC_IRQHandler(void)       WEAK_ALIAS(irq_undefined)
+void TIM2_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void TIM3_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void TIM4_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void I2C1_EV_IRQHandler(void)       WEAK_ALIAS(irq_undefined)
+void I2C1_ER_IRQHandler(void)       WEAK_ALIAS(irq_undefined)
+void I2C2_EV_IRQHandler(void)       WEAK_ALIAS(irq_undefined)
+void I2C2_ER_IRQHandler(void)       WEAK_ALIAS(irq_undefined)
+void SPI1_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void SPI2_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void USART1_IRQHandler(void)        WEAK_ALIAS(irq_undefined)
+void USART2_IRQHandler(void)        WEAK_ALIAS(irq_undefined)
+void USART3_IRQHandler(void)        WEAK_ALIAS(irq_undefined)
+void EXTI15_10_IRQHandler(void)     WEAK_ALIAS(irq_undefined)
+void RTC_Alarm_IRQHandler(void)     WEAK_ALIAS(irq_undefined)
+void OTG_FS_WKUP_IRQHandler(void)       WEAK_ALIAS(irq_undefined)
+void TIM8_BRK_TIM12_IRQHandler(void)        WEAK_ALIAS(irq_undefined)
+void TIM8_UP_TIM13_IRQHandler(void)     WEAK_ALIAS(irq_undefined)
+void TIM8_TRG_COM_TIM14_IRQHandler(void)        WEAK_ALIAS(irq_undefined)
+void TIM8_CC_IRQHandler(void)       WEAK_ALIAS(irq_undefined)
+void DMA1_Stream7_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void FSMC_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void SDIO_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void TIM5_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void SPI3_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void UART4_IRQHandler(void)     WEAK_ALIAS(irq_undefined)
+void UART5_IRQHandler(void)     WEAK_ALIAS(irq_undefined)
+void TIM6_DAC_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void TIM7_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void DMA2_Stream0_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void DMA2_Stream1_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void DMA2_Stream2_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void DMA2_Stream3_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void DMA2_Stream4_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void ETH_IRQHandler(void)       WEAK_ALIAS(irq_undefined)
+void ETH_WKUP_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void CAN2_TX_IRQHandler(void)       WEAK_ALIAS(irq_undefined)
+void CAN2_RX0_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void CAN2_RX1_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void CAN2_SCE_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void OTG_FS_IRQHandler(void)        WEAK_ALIAS(irq_undefined)
+void DMA2_Stream5_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void DMA2_Stream6_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void DMA2_Stream7_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void USART6_IRQHandler(void)        WEAK_ALIAS(irq_undefined)
+void I2C3_EV_IRQHandler(void)       WEAK_ALIAS(irq_undefined)
+void I2C3_ER_IRQHandler(void)       WEAK_ALIAS(irq_undefined)
+void OTG_HS_EP1_OUT_IRQHandler(void)        WEAK_ALIAS(irq_undefined)
+void OTG_HS_EP1_IN_IRQHandler(void)     WEAK_ALIAS(irq_undefined)
+void OTG_HS_WKUP_IRQHandler(void)       WEAK_ALIAS(irq_undefined)
+void OTG_HS_IRQHandler(void)        WEAK_ALIAS(irq_undefined)
+void DCMI_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void HASH_RNG_IRQHandler(void)      WEAK_ALIAS(irq_undefined)
+void FPU_IRQHandler(void)       WEAK_ALIAS(irq_undefined)
+
 
 /*****************************************************************************
  * Forward undefined fault handlers to an infinite loop function. The Handlers
@@ -115,66 +179,89 @@ const void *vectors[] SECTION(".irq-vectors") =
   PendSV_Handler,        // The PendSV handler
   SysTick_Handler,       // The SysTick handler
 
-  // Wakeup I/O pins handlers
-  WAKEUP_IRQHandler,     // PIO0_0  Wakeup
-  WAKEUP_IRQHandler,     // PIO0_1  Wakeup
-  WAKEUP_IRQHandler,     // PIO0_2  Wakeup
-  WAKEUP_IRQHandler,     // PIO0_3  Wakeup
-  WAKEUP_IRQHandler,     // PIO0_4  Wakeup
-  WAKEUP_IRQHandler,     // PIO0_5  Wakeup
-  WAKEUP_IRQHandler,     // PIO0_6  Wakeup
-  WAKEUP_IRQHandler,     // PIO0_7  Wakeup
-  WAKEUP_IRQHandler,     // PIO0_8  Wakeup
-  WAKEUP_IRQHandler,     // PIO0_9  Wakeup
-  WAKEUP_IRQHandler,     // PIO0_10 Wakeup
-  WAKEUP_IRQHandler,     // PIO0_11 Wakeup
-  WAKEUP_IRQHandler,     // PIO1_0  Wakeup
-  WAKEUP_IRQHandler,     // PIO1_1  Wakeup
-  WAKEUP_IRQHandler,     // PIO1_2  Wakeup
-  WAKEUP_IRQHandler,     // PIO1_3  Wakeup
-  WAKEUP_IRQHandler,     // PIO1_4  Wakeup
-  WAKEUP_IRQHandler,     // PIO1_5  Wakeup
-  WAKEUP_IRQHandler,     // PIO1_6  Wakeup
-  WAKEUP_IRQHandler,     // PIO1_7  Wakeup
-  WAKEUP_IRQHandler,     // PIO1_8  Wakeup
-  WAKEUP_IRQHandler,     // PIO1_9  Wakeup
-  WAKEUP_IRQHandler,     // PIO1_10 Wakeup
-  WAKEUP_IRQHandler,     // PIO1_11 Wakeup
-  WAKEUP_IRQHandler,     // PIO2_0  Wakeup
-  WAKEUP_IRQHandler,     // PIO2_1  Wakeup
-  WAKEUP_IRQHandler,     // PIO2_2  Wakeup
-  WAKEUP_IRQHandler,     // PIO2_3  Wakeup
-  WAKEUP_IRQHandler,     // PIO2_4  Wakeup
-  WAKEUP_IRQHandler,     // PIO2_5  Wakeup
-  WAKEUP_IRQHandler,     // PIO2_6  Wakeup
-  WAKEUP_IRQHandler,     // PIO2_7  Wakeup
-  WAKEUP_IRQHandler,     // PIO2_8  Wakeup
-  WAKEUP_IRQHandler,     // PIO2_9  Wakeup
-  WAKEUP_IRQHandler,     // PIO2_10 Wakeup
-  WAKEUP_IRQHandler,     // PIO2_11 Wakeup
-  WAKEUP_IRQHandler,     // PIO3_0  Wakeup
-  WAKEUP_IRQHandler,     // PIO3_1  Wakeup
-  WAKEUP_IRQHandler,     // PIO3_2  Wakeup
-  WAKEUP_IRQHandler,     // PIO3_3  Wakeup
-
-  // Specific peripheral irq handlers
-  I2C_IRQHandler,        // I2C0
-  TIMER16_0_IRQHandler,  // CT16B0 (16-bit Timer 0)
-  TIMER16_1_IRQHandler,  // CT16B1 (16-bit Timer 1)
-  TIMER32_0_IRQHandler,  // CT32B0 (32-bit Timer 0)
-  TIMER32_1_IRQHandler,  // CT32B1 (32-bit Timer 1)
-  SSP_IRQHandler,        // SSP0
-  UART_IRQHandler,       // UART0
-  USB_IRQHandler,        // USB IRQ
-  USB_FIQHandler,        // USB FIQ
-  ADC_IRQHandler,        // ADC (A/D Converter)
-  WDT_IRQHandler,        // WDT (Watchdog Timer)
-  BOD_IRQHandler,        // BOD (Brownout Detect)
-  FMC_IRQHandler,        // Flash (IP2111 Flash Memory Controller)
-  PIOINT3_IRQHandler,    // PIO INT3
-  PIOINT2_IRQHandler,    // PIO INT2
-  PIOINT1_IRQHandler,    // PIO INT1
-  PIOINT0_IRQHandler,    // PIO INT0
+  /* External Interrupts */
+  WWDG_IRQHandler,                   /* Window WatchDog              */
+  PVD_IRQHandler,                    /* PVD through EXTI Line detection */
+  TAMP_STAMP_IRQHandler,             /* Tamper and TimeStamps through the EXTI line */
+  RTC_WKUP_IRQHandler,               /* RTC Wakeup through the EXTI line */
+  FLASH_IRQHandler,                  /* FLASH                        */
+  RCC_IRQHandler,                    /* RCC                          */
+  EXTI0_IRQHandler,                  /* EXTI Line0                   */
+  EXTI1_IRQHandler,                  /* EXTI Line1                   */
+  EXTI2_IRQHandler,                  /* EXTI Line2                   */
+  EXTI3_IRQHandler,                  /* EXTI Line3                   */
+  EXTI4_IRQHandler,                  /* EXTI Line4                   */
+  DMA1_Stream0_IRQHandler,           /* DMA1 Stream 0                */
+  DMA1_Stream1_IRQHandler,           /* DMA1 Stream 1                */
+  DMA1_Stream2_IRQHandler,           /* DMA1 Stream 2                */
+  DMA1_Stream3_IRQHandler,           /* DMA1 Stream 3                */
+  DMA1_Stream4_IRQHandler,           /* DMA1 Stream 4                */
+  DMA1_Stream5_IRQHandler,           /* DMA1 Stream 5                */
+  DMA1_Stream6_IRQHandler,           /* DMA1 Stream 6                */
+  ADC_IRQHandler,                    /* ADC1, ADC2 and ADC3s         */
+  CAN1_TX_IRQHandler,                /* CAN1 TX                      */
+  CAN1_RX0_IRQHandler,               /* CAN1 RX0                     */
+  CAN1_RX1_IRQHandler,               /* CAN1 RX1                     */
+  CAN1_SCE_IRQHandler,               /* CAN1 SCE                     */
+  EXTI9_5_IRQHandler,                /* External Line[9:5]s          */
+  TIM1_BRK_TIM9_IRQHandler,          /* TIM1 Break and TIM9          */
+  TIM1_UP_TIM10_IRQHandler,          /* TIM1 Update and TIM10        */
+  TIM1_TRG_COM_TIM11_IRQHandler,     /* TIM1 Trigger and Commutation and TIM11 */
+  TIM1_CC_IRQHandler,                /* TIM1 Capture Compare         */
+  TIM2_IRQHandler,                   /* TIM2                         */
+  TIM3_IRQHandler,                   /* TIM3                         */
+  TIM4_IRQHandler,                   /* TIM4                         */
+  I2C1_EV_IRQHandler,                /* I2C1 Event                   */
+  I2C1_ER_IRQHandler,                /* I2C1 Error                   */
+  I2C2_EV_IRQHandler,                /* I2C2 Event                   */
+  I2C2_ER_IRQHandler,                /* I2C2 Error                   */
+  SPI1_IRQHandler,                   /* SPI1                         */
+  SPI2_IRQHandler,                   /* SPI2                         */
+  USART1_IRQHandler,                 /* USART1                       */
+  USART2_IRQHandler,                 /* USART2                       */
+  USART3_IRQHandler,                 /* USART3                       */
+  EXTI15_10_IRQHandler,              /* External Line[15:10]s        */
+  RTC_Alarm_IRQHandler,              /* RTC Alarm (A and B) through EXTI Line */
+  OTG_FS_WKUP_IRQHandler,            /* USB OTG FS Wakeup through EXTI line */
+  TIM8_BRK_TIM12_IRQHandler,         /* TIM8 Break and TIM12         */
+  TIM8_UP_TIM13_IRQHandler,          /* TIM8 Update and TIM13        */
+  TIM8_TRG_COM_TIM14_IRQHandler,     /* TIM8 Trigger and Commutation and TIM14 */
+  TIM8_CC_IRQHandler,                /* TIM8 Capture Compare         */
+  DMA1_Stream7_IRQHandler,           /* DMA1 Stream7                 */
+  FSMC_IRQHandler,                   /* FSMC                         */
+  SDIO_IRQHandler,                   /* SDIO                         */
+  TIM5_IRQHandler,                   /* TIM5                         */
+  SPI3_IRQHandler,                   /* SPI3                         */
+  UART4_IRQHandler,                  /* UART4                        */
+  UART5_IRQHandler,                  /* UART5                        */
+  TIM6_DAC_IRQHandler,               /* TIM6 and DAC1&2 underrun errors */
+  TIM7_IRQHandler,                   /* TIM7                         */
+  DMA2_Stream0_IRQHandler,           /* DMA2 Stream 0                */
+  DMA2_Stream1_IRQHandler,           /* DMA2 Stream 1                */
+  DMA2_Stream2_IRQHandler,           /* DMA2 Stream 2                */
+  DMA2_Stream3_IRQHandler,           /* DMA2 Stream 3                */
+  DMA2_Stream4_IRQHandler,           /* DMA2 Stream 4                */
+  ETH_IRQHandler,                    /* Ethernet                     */
+  ETH_WKUP_IRQHandler,               /* Ethernet Wakeup through EXTI line */
+  CAN2_TX_IRQHandler,                /* CAN2 TX                      */
+  CAN2_RX0_IRQHandler,               /* CAN2 RX0                     */
+  CAN2_RX1_IRQHandler,               /* CAN2 RX1                     */
+  CAN2_SCE_IRQHandler,               /* CAN2 SCE                     */
+  OTG_FS_IRQHandler,                 /* USB OTG FS                   */
+  DMA2_Stream5_IRQHandler,           /* DMA2 Stream 5                */
+  DMA2_Stream6_IRQHandler,           /* DMA2 Stream 6                */
+  DMA2_Stream7_IRQHandler,           /* DMA2 Stream 7                */
+  USART6_IRQHandler,                 /* USART6                       */
+  I2C3_EV_IRQHandler,                /* I2C3 event                   */
+  I2C3_ER_IRQHandler,                /* I2C3 error                   */
+  OTG_HS_EP1_OUT_IRQHandler,         /* USB OTG HS End Point 1 Out   */
+  OTG_HS_EP1_IN_IRQHandler,          /* USB OTG HS End Point 1 In    */
+  OTG_HS_WKUP_IRQHandler,            /* USB OTG HS Wakeup through EXTI */
+  OTG_HS_IRQHandler,                 /* USB OTG HS                   */
+  DCMI_IRQHandler,                   /* DCMI                         */
+  0,                                 /* CRYP crypto                  */
+  HASH_RNG_IRQHandler,               /* Hash and Rng                 */
+  FPU_IRQHandler,                    /* FPU                          */
 };
 
 /******************************************************************************
