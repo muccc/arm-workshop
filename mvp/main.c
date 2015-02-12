2c4a2fda4475c84a73ae07c169714d99ea41dac3
Tobias Schneider <schneider@blinkenlichts.net>
2015-02-12 20:59:28 +0100

    feat(main): Turn on an LED

--- mvp/main.c
+++ mvp/main.c
@@ -3,7 +3,24 @@
 
 #include <stdbool.h>
 
+#define LED4_PIN                         GPIO_PIN_12
+#define LED4_GPIO_PORT                   GPIOD
+
 int main(void)
 {
+    GPIO_InitTypeDef   GPIO_InitStructure;
+
+    /* Enable GPIOD clock */
+    __HAL_RCC_GPIOD_CLK_ENABLE();
+
+    /* Configure PD12 pin as output */
+    GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStructure.Pin = LED4_PIN;
+    GPIO_InitStructure.Pull = GPIO_PULLUP;
+    GPIO_InitStructure.Speed = GPIO_SPEED_FAST;
+    HAL_GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+    HAL_GPIO_WritePin(LED4_GPIO_PORT, LED4_PIN, GPIO_PIN_SET);
+
     while(1);
 }
