9399a7d610cf53835f0539fea4a5413f00115533
bsx <bsx+ccc@0xcafec0.de>
2015-02-12 20:17:23 +0100

    chore(mvp): cleanup Makefile, add startup code

--- mvp/main.c
+++ mvp/main.c
@@ -1,38 +1,9 @@
-#include "lib/stm32f0xx_rcc.h"
-#include "lib/stm32f0xx_gpio.h"
+#include "stm32f4xx_hal_rcc.h"
+#include "stm32f4xx_hal_gpio.h"
 
 #include <stdbool.h>
-
-#define STACK_TOP 0x20000800
-
-//Declarations
-void nmi_handler(void);
-void hardfault_handler(void);
-int main(void);
-
-// Define the vector table
-unsigned int * myvectors[4]
-   __attribute__ ((section("vectors")))= {
-   	(unsigned int *)	STACK_TOP,	// stack pointer
-   	(unsigned int *) 	main,		// code entry point
-   	(unsigned int *)	nmi_handler,		// NMI handler (not really)
-   	(unsigned int *)	hardfault_handler		// hard fault handler (let's hope not)
-};
-
-void assert_param(bool x)
-{
-}
 
 int main(void)
 {
     while(1);
-}
-void nmi_handler(void)
-{
-	return ;
-}
-
-void hardfault_handler(void)
-{
-	return ;
 }
